e9fcc0eb6e3cd16f426c0a1564e6029381884938
milena-rocha <67220184+milenasrocha@users.noreply.github.com>
2023-10-21 04:32:47 -0300

    Inventory UI tween on KeyDown

--- Project/Assets/Scripts/Inventory/UI/Inventory.cs
+++ Project/Assets/Scripts/Inventory/UI/Inventory.cs
@@ -1,21 +1,37 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using DG.Tweening;
 
 namespace UI.Inventory
 {
     public class Inventory : Singleton<Inventory>
     {
-        [SerializeField] Transform _plantsHolder;
+        [SerializeField] bool isOpen = false;
+        [Space]
+        float tweenStartPositionY;
+        public float tweenTargetPositionY;
+        [Space]
+        [SerializeField] Transform _plantsContent;
         [SerializeField] GameObject _plantPrefab;
         Dictionary<string, Plant> _plants;
-
+        [Space]
         [SerializeField] string _selected;
         public string currentPlant => _selected;
-
         public Action<string> onSelectedChanged;
 
+
+        protected override void Awake()
+        {
+            base.Awake();
+            tweenStartPositionY = transform.position.y;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+                SwitchState();
+        }
 
         void Start()
         {
@@ -30,12 +46,38 @@ namespace UI.Inventory
 
             foreach (KeyValuePair<string, PlantStatic> item in PlantStaticsHolder.Instance.plantStatics)
             {
-                instance = MonoBehaviour.Instantiate(_plantPrefab, _plantsHolder);
+                instance = MonoBehaviour.Instantiate(_plantPrefab, _plantsContent);
 
                 Plant plant = instance.GetComponent<Plant>();
                 plant.Set(item.Key);
                 _plants.Add(item.Key, plant);
             }
+        }
+
+        public void SetState(bool b)
+        {
+            if (isOpen == b)
+                return;
+
+            SwitchState();
+        }
+        public void SwitchState()
+        {
+            isOpen = !isOpen;
+
+            if (isOpen)
+                Show();
+            else
+                Hide();
+        }
+        public void Show()
+        {
+            transform.DOMoveY(tweenTargetPositionY, .5f);
+        }
+
+        public void Hide()
+        {
+            transform.DOMoveY(tweenStartPositionY, .5f);
         }
 
         public void Select(string id)
@@ -47,7 +89,8 @@ namespace UI.Inventory
 
             _plants[_selected].OnSelected();
 
-            onSelectedChanged.Invoke(id);
+            if (onSelectedChanged != null)
+                onSelectedChanged.Invoke(id);
         }
     }
 }
