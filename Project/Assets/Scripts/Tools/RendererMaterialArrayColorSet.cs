52a323ab8553628e081d3ec9f4cef92a7e8b3d65
Alysson Victor <alyssonvitorpro@gmail.com>
2023-10-21 16:38:21 -0300

    Player prefab / Scene

---
+++ Project/Assets/Scripts/Tools/RendererMaterialArrayColorSet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RendererMaterialArrayColorSet : MonoBehaviour
+{
+    public string property = "_BaseColor";
+    public Color[] colors = new Color[1];
+
+
+    private void OnValidate()
+    {
+        UpadateColor();
+    }
+    private void Awake()
+    {
+        UpadateColor();
+    }
+
+    public void UpadateColor()
+    {
+        for (int i = 0; i < colors.Length; i++)
+        {
+            MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.GetPropertyBlock(propertyBlock, i);
+            propertyBlock.SetColor(property, colors[i]);
+                
+            renderer.SetPropertyBlock(propertyBlock, i);
+        }
+    }
+}
