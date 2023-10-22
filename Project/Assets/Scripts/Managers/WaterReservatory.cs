d7ccac2ff88cc5a08a4cb98ed966e57c49d92e48
milena-rocha <67220184+milenasrocha@users.noreply.github.com>
2023-10-22 13:34:06 -0300

    Water Reservatory level on mesh

--- Project/Assets/Scripts/Managers/WaterReservatory.cs
+++ Project/Assets/Scripts/Managers/WaterReservatory.cs
@@ -1,20 +1,50 @@
+using System;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class WaterReservatory : Singleton<WaterReservatory>
 {
     [SerializeField] private float _reservatoryCapacity = 100f;
     [SerializeField] private float _currentWaterAmount = 100f;
-    
+
+
+
+    [SerializeField] Transform waterTankLevel;
+    [Range(0, 1)][SerializeField] float _waterTankTweenDuration = .5f;
+
+    public Action<float> onAmountChanged;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    void Start()
+    {
+        waterTankLevel.localScale = new Vector3(1, GetWaterPercentage(), 1);
+    }
 
     public void AddWater(float water)
     {
         _currentWaterAmount = Mathf.Clamp(_currentWaterAmount + water, 0f, _reservatoryCapacity);
+
+        waterTankLevel.DOScale(new Vector3(1, GetWaterPercentage(), 1), _waterTankTweenDuration);
+
+        if (onAmountChanged != null)
+            onAmountChanged.Invoke(_currentWaterAmount);
     }
 
     public void RemoveWater(float water)
     {
          _currentWaterAmount = Mathf.Clamp(_currentWaterAmount - water, 0f, _reservatoryCapacity);
-         print(GetWaterPercentage() + "Percentage");
+
+        waterTankLevel.DOScale(new Vector3(1, GetWaterPercentage(), 1), _waterTankTweenDuration);
+
+        if (onAmountChanged != null)
+            onAmountChanged.Invoke(_currentWaterAmount);
+
+        print(GetWaterPercentage() + "Percentage");
     }
 
     public float CheckWaterAmount()
@@ -26,5 +56,4 @@ public class WaterReservatory : Singleton<WaterReservatory>
     {
         return _currentWaterAmount / _reservatoryCapacity * 100;
     }
-
 }
