6efdd51c8c116108a300052c3d208e166cf0394d
milena-rocha <67220184+milenasrocha@users.noreply.github.com>
2023-10-22 16:23:23 -0300

    Water Tank level

--- Project/Assets/Scripts/Managers/WaterTank.cs
+++ Project/Assets/Scripts/Managers/WaterTank.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class WaterTank : Singleton<WaterTank>
 {
-    [SerializeField] Transform waterTankLevel;
-    [Range(0, 1)][SerializeField] float _waterTankTweenDuration = .5f;
+    [SerializeField] Transform _levelTransform;
+    [Range(0, 1)][SerializeField] float levelTweenDuration = .5f;
 
     void Start()
     {
         WaterReservatory waterReservatory = WaterReservatory.Instance;
 
-        waterTankLevel.localScale = new Vector3(1, waterReservatory.GetWaterPercentage()/100, 1);
-        waterReservatory.onAmountChanged += (amount) => waterTankLevel.DOScale(new Vector3(1, waterReservatory.GetWaterPercentage()/100, 1), _waterTankTweenDuration);
+        _levelTransform.localScale = new Vector3(1, waterReservatory.GetWaterPercentage()/100, 1);
+        waterReservatory.onAmountChanged += (amount) => _levelTransform.DOScale(new Vector3(1, waterReservatory.GetWaterPercentage()/100, 1), levelTweenDuration);
     }
 }
